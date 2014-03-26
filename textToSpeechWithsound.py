4adc2161bf00d7d0e8b702a2b7f66f627423c14b
Carlos <cadiaz@csumb.edu>
2014-03-26 14:32:14 -0700

    Current

--- textToSpeechWithsound.py
+++ textToSpeechWithsound.py
@@ -51,6 +51,10 @@ def createWordSound(word):
   llFilePath="/Users/bobby/textospeech/LL.wav"
   loFilePath="/Users/bobby/textospeech/LO.wav"
   
+  coFIlePath="/Users/bobby/textospeech/CO.wav"
+  olFilePath="/Users/bobby/textospeech/OL.wav"
+  
+      
   #making the sound files
   heSound=makeSound(heFilePath)
   llSound=makeSound(llFilePath)
@@ -60,6 +64,16 @@ def createWordSound(word):
   helen=getLength(heSound)
   lllen=getLength(llSound)
   lolen=getLength(loSound)
+  
+  
+    #making the sound files
+  coSound=makeSound(coFIlePath)
+  olSound=makeSound(olFilePath)
+  
+  
+  #getting the leght of eahc file
+  colen=getLength(coSound)
+  ollen=getLength(olSound)
   
   
   
@@ -74,31 +88,56 @@ def createWordSound(word):
         hesampleValue=getSampleValueAt(heSound,i)
         setSampleValueAt(wordSound,wordIndex,hesampleValue)
         wordIndex = wordIndex+1
-        # testing other dinamic funciton
-       # wordToS
-      #wavFilePath.append("/Users/amitpanchal/Desktop/GitHub Files/textospeech/HE.wav")
-     # tempSound = makeSound(wavFilePath[i])
-      #samplingRates.append(getSamplingRate(tempSound)) 
+       
     elif(word[i].lower() == "ll"):
       for i in range(0,helen):
         llsampleValue=getSampleValueAt(llSound,i)
         setSampleValueAt(wordSound,wordIndex,llsampleValue)
         wordIndex = wordIndex+1
-     # wavFilePath.append("/Users/amitpanchal/Desktop/GitHub Files/textospeech/LL.wav")
-      #sound.append(makeSound(wavFilePath[i]))
+   
     elif(word[i].lower() == "o"):
       for i in range(0,lolen):
         losampleValue=getSampleValueAt(loSound,i)
         #print "it enter this part"
         setSampleValueAt(wordSound,wordIndex,losampleValue)
         wordIndex = wordIndex+1
-     # wavFilePath.append("/Users/amitpanchal/Desktop/GitHub Files/textospeech/O.wav")
-      #sound.append(makeSound(wavFilePath[i]))
-    #else:
-     # print "error"  
-  #print wavFiles
+        
+    elif(word[i].lower() == "co"):
+      for i in range(0,colen):
+        losampleValue=getSampleValueAt(coSound,i)
+        #print "it enter this part"
+        setSampleValueAt(wordSound,wordIndex,losampleValue)
+        wordIndex = wordIndex+1
+        
+        
+    elif(word[i].lower() == "ol"):
+      for i in range(0,ollen):
+        losampleValue=getSampleValueAt(olSound,i)
+        #print "it enter this part"
+        setSampleValueAt(wordSound,wordIndex,losampleValue)
+        wordIndex = wordIndex+1    
+   
+        
   
-  play(wordSound)
+  
+  
+  chooseSpeed(wordSound, 3)
+  
+  
+def chooseSpeed(sound, pace):
+
+ target=makeEmptySound(getLength(sound)) #creating new sound with the length of original sound
+ sourceIndex=0
+ 
+ for targetIndex in range(0, getLength(target)/pace):  #dividing by pace makes it only play one time rather than multiple times
+   sourceValue=getSampleValueAt(sound, int(sourceIndex))  #getting original sound sample value
+   setSampleValueAt(target, targetIndex, sourceValue)  #setting the new sound parameters
+   sourceIndex+=pace  #increase the index based on the pace that was set to stay at constant rate.
+   
+   if(sourceIndex >= getLength(sound)): #for error handling if index is greater than 0
+    sourceIndex=0
+    
+ play(target) 
    
 def main():
   userInput = raw_input("Please Enter a Word: ")
